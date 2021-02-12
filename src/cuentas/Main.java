cfd556df17323546488cf2caebc28b4a9d2dd1b7
rodri <rodri@DESKTOP-0D0E9TP>
2021-02-12 17:20:28 +0100

    Commit inicial

---
+++ src/cuentas/Main.java
@@ -0,0 +1,29 @@
+package cuentas;
+
+public class Main {
+
+    public static void main(String[] args) {
+        CCuenta cuenta1;
+        double saldoActual;
+
+        cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        operaciones_cuenta(cuenta1, 2300);
+    }
+
+    private static void operaciones_cuenta(CCuenta cuenta1, float cantidad) {
+        double saldoActual;
+        saldoActual = cuenta1.estado();
+        System.out.println("El saldo actual es"+ saldoActual );
+        try {
+            cuenta1.retirar(cantidad);
+        } catch (Exception e) {
+            System.out.print("Fallo al retirar");
+        }
+        try {
+            System.out.println("Ingreso en cuenta");
+            cuenta1.ingresar(cantidad);
+        } catch (Exception e) {
+            System.out.print("Fallo al ingresar");
+        }
+    }
+}
